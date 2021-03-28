45a336ac690ba303496d5581c70f79f92a9a5c6f
Davida Garner <davidac.garner@gmail.com>
2021-03-27 20:28:49 -0400

    first commit

---
+++ public/main.js
@@ -0,0 +1,142 @@
+// worked with hayden and cass
+
+var thumbs = document.getElementsByClassName("thumbs");
+var trash = document.getElementsByClassName("fa-trash-o");
+
+Array.from(thumbs).forEach(function(element) {
+      element.addEventListener('click', function(e){
+        const name = this.parentNode.parentNode.childNodes[1].innerText
+        const src = this.parentNode.parentNode.childNodes[3].getAttribute("src")
+        // const msg = this.parentNode.parentNode.childNodes[5].innerText
+        const value = parseFloat(this.parentNode.parentNode.childNodes[7].innerText)
+        if(e.target.classList.contains("fa-thumbs-up")){
+             fetch('messages', {
+              method: 'put',
+              headers: {'Content-Type': 'application/json'},
+              body: JSON.stringify({
+                 'name': name,
+                 'src': src,
+                //  'msg': msg,
+                 'value':value,
+                 'thumbsUp': "yes",
+                 'thumbsDown': ""
+                  })
+             })
+             .then(response => {
+               if (response.ok) return response.json()
+             })
+             .then(data => {
+               window.location.reload(true)
+             })
+      }else if (e.target.classList.contains("fa-thumbs-down")){
+            fetch('messages', {
+              method: 'put',
+              headers: {'Content-Type': 'application/json'},
+              body: JSON.stringify({
+                  'name': name,
+                  'src': src,
+                  // 'msg': msg,
+                  'value':value,
+                  'thumbsUp': "",
+                  'thumbsDown': "yes"
+                  })
+            })
+            .then(response => {
+              if (response.ok) return response.json()
+            })
+            .then(data => {
+              window.location.reload(true)
+            })
+      }
+
+      });
+});
+
+Array.from(trash).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const name = this.parentNode.parentNode.childNodes[1].innerText
+        // const msg = this.parentNode.parentNode.childNodes[5].innerText
+        fetch('messages', {
+          method: 'delete',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            'name': name,
+            // 'msg': msg
+          })
+        }).then(function (response) {
+          window.location.reload()
+        })
+      });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var thumbUp = document.getElementsByClassName("fa-thumbs-up");
+// var trash = document.getElementsByClassName("fa-trash");
+
+// Array.from(thumbUp).forEach(function(element) {
+//       element.addEventListener('click', function(){
+//         const name = this.parentNode.parentNode.childNodes[1].innerText
+//         const msg = this.parentNode.parentNode.childNodes[3].innerText
+//         const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+//         fetch('messages', {
+//           method: 'put',
+//           headers: {'Content-Type': 'application/json'},
+//           body: JSON.stringify({
+//             'name': name,
+//             'msg': msg,
+//             'thumbUp':thumbUp
+//           })
+//         })
+//         .then(response => {
+//           if (response.ok) return response.json()
+//         })
+//         .then(data => {
+//           console.log(data)
+//           window.location.reload(true)
+//         })
+//       });
+// });
+
+// Array.from(trash).forEach(function(element) {
+//       element.addEventListener('click', function(){
+//         const name = this.parentNode.parentNode.childNodes[1].innerText
+//         const msg = this.parentNode.parentNode.childNodes[3].innerText
+//         fetch('messages', {
+//           method: 'delete',
+//           headers: {
+//             'Content-Type': 'application/json'
+//           },
+//           body: JSON.stringify({
+//             'name': name,
+//             'msg': msg
+//           })
+//         }).then(function (response) {
+//           window.location.reload()
+//         })
+//       });
+// });
